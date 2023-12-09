9fe2f36eee3406328365ffe4f6360c2d3f3535f1
User <paseidon72@gmail.com>
2023-12-09 16:02:36 +0200

    Initial commit

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,3 +1,4 @@
+from utils.logger import Logger
 import requests
 
 """Список HTTP методов"""
@@ -8,20 +9,28 @@ class Http_methods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
